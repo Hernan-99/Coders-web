50d37940193fd3558844fb368d68163f3071a426
HernanGabriel <hernan24744@gmail.com>
2023-06-16 07:49:04 -0300

    app en angular, inicio del back

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  { path: 'index', component: HomeComponent },
+  { path: '', redirectTo: 'index', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/shared/header/header.component';
+import { FooterComponent } from './layouts/shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
